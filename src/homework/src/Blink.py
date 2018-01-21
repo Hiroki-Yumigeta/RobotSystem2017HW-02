92cd908bf90501e468cfbc75e6d4d990d2e94183
Hiroki-Yumigeta <s1526122TM@s.chibakoudai.jp>
2018-01-22 03:14:28 +0900

    fix LED.py Blink.py

--- src/homework/src/Blink.py
+++ src/homework/src/Blink.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# import
 import rospy
 from std_msgs.msg import Int32MultiArray
 
-n = 0
 
+# LEDに対応する行列の定義
 mat=[
     [0,0,0,0,0,0,0,0],
     [0,1,0,0,0,0,1,0],
@@ -15,22 +17,33 @@ mat=[
     [0,1,0,0,0,0,1,0],
     [0,0,0,0,0,0,0,0]
 ]
-
+# mainプログラム
 if __name__ == '__main__':
-    rospy.init_node('Blink') # なんていうプログラムか
+    rospy.init_node('Blink') # なんていうプログラムか ノードの定義
+
+    #パブリッシャの定義
     pub = rospy.Publisher('BlinkPub', Int32MultiArray, queue_size=100)
-    rate = rospy.Rate(1000) # 毎秒10回 回す
-    dataline = Int32MultiArray()
+
+    rate = rospy.Rate(1000) # 毎秒1000回 回す
+    dataline = Int32MultiArray() # メッセージのタイプ
+
+    # ループ
+    n = 0
     while not rospy.is_shutdown(): # rospyが落ちていない場合は
-        dataline.data = [len(mat)]
+
+        # 送信データの定義
+        dataline.data = [len(mat)] # matの行数の格納
         for i in xrange(len(mat)):
             dataline.data.extend(mat[i])
+
+        # データ送信
         pub.publish(dataline)
         rate.sleep() # 指定した分だけ休む
 
-        if n==500:
+
+        if n==500: # 500回に1度データの更新それ以外はそのまま次のループに移る
             for i in xrange(len(mat)):
                 for j in xrange(len(mat[0])):
-                    mat[i][j] = 1 if mat[i][j]==0 else 0
+                    mat[i][j] = 1 if mat[i][j]==0 else 0 # 1, 0の入れ替え
             n=0
         n+=1
--- src/homework/src/LED.py
+++ src/homework/src/LED.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+# import
 import rospy
 from std_msgs.msg import Int32MultiArray
 import wiringpi as w
 
+# ドットマトリクスLEDのピンに対応するGPIOの定義
 anode   = [21,20,16,12,25,24,23,18]
 cathode = [26,19,13, 6, 5,22,27,17]
 
+# 関数の定義
+# GPIOの初期化(OUTPUT)
 def initGPIO():
     for i in xrange(len(anode)):
-        w.pinMode(anode[i], w.OUTPUT)
+        w.pinMode(anode[i], w.OUTPUT)   # anode側
     for i in xrange(len(cathode)):
-        w.pinMode(cathode[i], w.OUTPUT)
+        w.pinMode(cathode[i], w.OUTPUT) # cathode側
+
+# LEDをオフにする
 def ResetPin():
     for i in xrange(len(anode)):
-        w.digitalWrite(anode[i],   w.LOW)
-        w.digitalWrite(cathode[i], w.HIGH)
+        w.digitalWrite(anode[i],   w.LOW)  # anode側はLOW
+        w.digitalWrite(cathode[i], w.HIGH) # cathode側はHIGH
 
+# データに基づきLEDを点灯
 def WriteLED(row, vector):
+    # anode(行)
     w.digitalWrite(anode[row], w.HIGH)
+    #  cathode(列)
     for i in xrange(len(vector)):
         w.digitalWrite(cathode[i], w.LOW if vector[i]==0 else w.HIGH)
 
+# 送られてきたデータの処理
 def cb(message):
-    # 中身
-    message_data = list(message.data)
-    split = message_data[0]
-    message_data.pop(0)
-    message_len = len(message_data)
-    for j in xrange(message_len/split):
+    type(message.data)
+    message_data = list(message.data) # list型に変換
+    split = message_data[0] # 先頭のデータ（行数）を取得
+    message_data.pop(0) #先頭のデータを削除
+
+    for j in xrange(split):
+
+        # 1行分のデータを取得
         vector = [message_data[j*split+0]]
         for i in range(1,split):
             vector.extend([message_data[j*split+i]])
-        WriteLED(j, vector)
-        rate.sleep()
-        ResetPin()
+         
+        WriteLED(j, vector) # 関数WriteLEDに渡す
+        rate.sleep() # 指定時間wait
+        ResetPin() # PINのリセット
 
+# mainプログラム
 if __name__ == '__main__':
-    rospy.init_node('LED')
-    rate = rospy.Rate(10000)
+    rospy.init_node('LED') # ノードの定義
+    rate = rospy.Rate(10000) #1秒間に10000回処理
+
+    # GPIO初期化
     w.wiringPiSetupGpio()
     initGPIO()
     ResetPin()
+
+    # サブスクライバの設定
     sub = rospy.Subscriber('BlinkPub', Int32MultiArray, cb)
     rospy.spin()
